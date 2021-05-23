71ccf756dff3fb99201dcecae1ec97924f49b2e7
Sodgerel Mandakhnaran <sm8694@nyu.edu>
2021-05-23 21:07:21 +0800

    Add belt divisions

--- bjj_mongolian_rulebook/lib/durationPage.dart
+++ bjj_mongolian_rulebook/lib/durationPage.dart
@@ -7,6 +7,7 @@ class DurationPage extends StatefulWidget {
 
 class _DurationPageState extends State<DurationPage> {
   String dropdownValue = 'Adult';
+  String dropdownValueBelt = 'White';
   List<String> divisionList = [
     'Kids 1',
     'Kids 2',
@@ -19,6 +20,7 @@ class _DurationPageState extends State<DurationPage> {
     'Master'
   ];
   List<int> ageList = [4, 6, 8, 10, 12, 14, 16, 18, 30];
+  List<String> beltList = ['White', 'Blue', 'Purple', 'Brown', 'Black'];
   int age = 18;
 
   @override
@@ -41,7 +43,83 @@ class _DurationPageState extends State<DurationPage> {
             child: buildDropdownButton(),
           ),
         ),
+        ListTile(
+          title: Text(
+            'BELT',
+            style: TextStyle(
+                fontSize: 16,
+                fontFamily: 'FreeSans',
+                fontWeight: FontWeight.bold),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: divisionList.indexOf(dropdownValue) > 6
+              ? Card(child: buildBeltDropdownButton())
+              : Card(
+                  elevation: 2,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Container(
+                      height: 16,
+                      child: Text(
+                        'All Belts',
+                        style: TextStyle(
+                          color: Colors.deepPurple,
+                          fontSize: 15,
+                          fontFamily: 'FreeSans',
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+        ),
+        ListTile(
+          title: Text(
+            'DURATION',
+            style: TextStyle(
+                fontSize: 16,
+                fontFamily: 'FreeSans',
+                fontWeight: FontWeight.bold),
+          ),
+        ),
       ],
+    );
+  }
+
+  Widget buildBeltDropdownButton() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            child: DropdownButton<String>(
+              value: dropdownValueBelt,
+              icon: const Icon(Icons.arrow_downward),
+              iconSize: 24,
+              elevation: 16,
+              style: const TextStyle(
+                  color: Colors.deepPurple, fontFamily: 'FreeSans'),
+              underline: Container(
+                height: 2,
+                color: Colors.deepPurpleAccent,
+              ),
+              onChanged: (String newValue) {
+                setState(() {
+                  dropdownValueBelt = newValue;
+                });
+              },
+              items: beltList.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
